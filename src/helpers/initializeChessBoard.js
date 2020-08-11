7246c1c5b78a35bb216ac561b3f9386c91838de1
Zack C <claar.zack@gmail.com>
2020-08-11 17:16:07 -0600

    Knight logic works on class file and renders on game board

--- src/helpers/initializeChessBoard.js
+++ src/helpers/initializeChessBoard.js
@@ -1,5 +1,6 @@
 import King from '../pieces/king.js';
 import Queen from '../pieces/queen.js';
+import Knight from '../pieces/knight.js';
 
 export default function InitializeChessboard() {
     // Initialize a squares array with a length of 64 and fill values with null
@@ -12,6 +13,11 @@ export default function InitializeChessboard() {
 
     squares[3] = new Queen(2);
     squares[59] = new Queen(1);
+    //1, 6, 57, 62
+    squares[1] = new Knight(2);
+    squares[6] = new Knight(2);
+    squares[57] = new Knight(1);
+    squares[62] = new Knight(1);
 
     return squares;
 }
---
+++ src/pieces/knight.js
@@ -0,0 +1,25 @@
+import Piece from './piece.js';
+
+export default class Knight extends Piece {
+
+    constructor(player) {
+        super(player, (player === 1 ? "https://upload.wikimedia.org/wikipedia/commons/7/70/Chess_nlt45.svg" 
+            : "https://upload.wikimedia.org/wikipedia/commons/e/ef/Chess_ndt45.svg"));
+    }
+
+    isMovePossible(startSquare, endSquare) {
+        return (startSquare + 6 === endSquare ||
+                startSquare - 6 === endSquare ||
+                startSquare + 10 === endSquare ||
+                startSquare - 10 === endSquare ||
+                startSquare + 15 === endSquare ||
+                startSquare - 15 === endSquare ||
+                startSquare + 17 === endSquare ||
+                startSquare - 17 === endSquare);
+    }
+
+    // will always return empty array because knight doesnt move in linear directions
+    getMovePath(startSquare, endSquare) {
+        return [];
+    }
+}
